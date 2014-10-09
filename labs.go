1bddb0cfd1bf40e27f242b1d19bbd378b17bbbb9
Tjerk Santegoeds <tsan@tdias.com>
2014-10-09 21:42:27 +0200

    Add support for historical positon ratios

--- labs.go
+++ labs.go
@@ -2,7 +2,9 @@ package oanda
 
 import (
 	"encoding/json"
+	"fmt"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -54,6 +56,7 @@ func (ce *CalendarEvent) UnmarshalJSON(data []byte) error {
 }
 
 func (c *Client) Calendar(instrument string, period Period) ([]CalendarEvent, error) {
+	instrument = strings.ToUpper(instrument)
 	req, err := c.NewRequest("GET", "/labs/v1/calendar", nil)
 	if err != nil {
 		return nil, err
@@ -74,4 +77,64 @@ func (c *Client) Calendar(instrument string, period Period) ([]CalendarEvent, er
 		return nil, err
 	}
 	return ces, nil
+}
+
+type PositionRatio struct {
+	Timestamp    time.Time
+	LongRatio    float64
+	ExchangeRate float64
+}
+
+type PositionRatios struct {
+	Instrument  string
+	DisplayName string
+	Ratios      []PositionRatio
+}
+
+func (c *Client) HistoricPositionRatios(instrument string, period Period) (*PositionRatios, error) {
+	instrument = strings.ToUpper(instrument)
+	req, err := c.NewRequest("GET", "/labs/v1/historical_position_ratios", nil)
+	if err != nil {
+		return nil, err
+	}
+	q := req.URL.Query()
+	q.Set("instrument", instrument)
+	q.Set("period", strconv.Itoa(int(period)))
+	req.URL.RawQuery = q.Encode()
+
+	rsp, err := c.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer rsp.Body.Close()
+
+	pr := PositionRatios{
+		Instrument: instrument,
+	}
+
+	v := struct {
+		Data map[string]struct {
+			Data  [][]float64 `json:"data"`
+			Label string      `json:"label"`
+		} `json:"data"`
+	}{}
+
+	dec := json.NewDecoder(rsp.Body)
+	if err = dec.Decode(&v); err != nil {
+		return nil, err
+	}
+	data, ok := v.Data[instrument]
+	if !ok || len(data.Data) == 0 {
+		return nil, fmt.Errorf("No HistoricPositionRatios found for instrument %s", instrument)
+	}
+
+	pr.DisplayName = data.Label
+	pr.Ratios = make([]PositionRatio, len(data.Data))
+	for i, ratio := range data.Data {
+		pr.Ratios[i].Timestamp = time.Unix(int64(ratio[0]), 0)
+		pr.Ratios[i].LongRatio = ratio[1]
+		pr.Ratios[i].ExchangeRate = ratio[2]
+	}
+
+	return &pr, nil
 }
--- labs_test.go
+++ labs_test.go
@@ -1,7 +1,7 @@
 package oanda_test
 
 import (
-	"os"
+	"strings"
 
 	"github.com/santegoeds/oanda"
 
@@ -15,18 +15,23 @@ type TestLabsSuite struct {
 var _ = check.Suite(&TestLabsSuite{})
 
 func (ts *TestLabsSuite) SetUpSuite(c *check.C) {
-	token := os.Getenv("FXPRACTICE_TOKEN")
-	if token == "" {
-		c.Skip("FXPRACTICE_TOKEN is not set")
-	} else {
-		client, err := oanda.NewFxPracticeClient(token)
-		c.Assert(err, check.IsNil)
-		ts.c = client
-	}
+	ts.c = NewTestClient(c, false)
 }
 
-func (ts *TestLabsSuite) TestCalendar(c *check.C) {
+func (ts *TestLabsSuite) TestLabsCalendar(c *check.C) {
 	events, err := ts.c.Calendar("eur_usd", oanda.Year)
 	c.Assert(err, check.IsNil)
 	c.Log(events)
+	c.Assert(len(events) > 0, check.Equals, true)
+}
+
+func (ts *TestLabsSuite) TestLabsHistoricPositionRatios(c *check.C) {
+	instrument := "eur_usd"
+	ratios, err := ts.c.HistoricPositionRatios("eur_usd", oanda.Year)
+	c.Assert(err, check.IsNil)
+	c.Log(ratios)
+	instrument = strings.ToUpper(instrument)
+	c.Assert(ratios.Instrument, check.Equals, instrument)
+	c.Assert(ratios.DisplayName, check.Equals, strings.Replace(instrument, "_", "/", -1))
+	c.Assert(len(ratios.Ratios) > 0, check.Equals, true)
 }
